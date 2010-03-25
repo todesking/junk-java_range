7fcddd985bd1c6ad534363069f69b65074185408
todesking <discommunicative@gmail.com>
2010-03-25 23:34:18 +0900

    add testcase(step is minus)

--- test/com/todesking/sandbox/RangeTest.java
+++ test/com/todesking/sandbox/RangeTest.java
@@ -11,5 +11,6 @@ public class RangeTest {
 	@Test
 	public void range() {
 		assertThat(Range.range(0, 5, 1).toList(),is(Arrays.asList(0,1,2,3,4,5)));
+		assertThat(Range.range(0, -5, -1).toList(),is(Arrays.asList(0,-1,-2,-3,-4,-5)));
 	}
 }
