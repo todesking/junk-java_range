58792217ed1ce5ac323bd5cfe301b850e2f8cbbf
todesking <discommunicative@gmail.com>
2010-03-25 23:38:54 +0900

    refactor test

--- test/com/todesking/sandbox/RangeTest.java
+++ test/com/todesking/sandbox/RangeTest.java
@@ -4,14 +4,19 @@ import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Test;
 
 public class RangeTest {
 	@Test
 	public void range() {
-		assertThat(Range.range(0, 5, 1).toList(),is(Arrays.asList(0,1,2,3,4,5)));
-		assertThat(Range.range(0, -5, -1).toList(),is(Arrays.asList(0,-1,-2,-3,-4,-5)));
-		assertThat(Range.range(0, 6, 2).toList(),is(Arrays.asList(0,2,4,6)));
+		assertThat(Range.range(0, 5, 1).toList(),is(list(0,1,2,3,4,5)));
+		assertThat(Range.range(0, -5, -1).toList(),is(list(0,-1,-2,-3,-4,-5)));
+		assertThat(Range.range(0, 6, 2).toList(),is(list(0,2,4,6)));
+	}
+
+	private List<Integer> list(Integer... args) {
+		return Arrays.asList(args);
 	}
 }
